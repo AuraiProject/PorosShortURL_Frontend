cb57c5cb616d409af31eec9f8dd85fbce6f70d3e
Arianxx <34032486+Arianxx@users.noreply.github.com>
2018-10-18 08:50:10 +0800

    feat:add the api doc page

--- src/containers/api/index.js
+++ src/containers/api/index.js
@@ -1,8 +1,81 @@
 import React, {Component} from 'react';
+import {Card} from 'antd';
+
+import {InputBox} from "../../components/inputBox";
+
+
+const docContent = {
+  title: "/api/url",
+  content: [
+    {
+      key: "GET",
+      tab: "GET",
+      content: (
+        <div>
+          <strong>Intro</strong>
+          <p>Get the full info from the existing short url.</p>
+          <br/>
+          <strong>Params</strong>
+          <ul>
+            <li>short_url: The short url that you want to get the info.</li>
+          </ul>
+          <br/>
+          <strong>Headers</strong>
+          <ul>
+            <li>Authentication: Basic %The short url's base64 encoded password.(optional)%</li>
+          </ul>
+        </div>
+      )
+    },
+    {
+      key: "POST",
+      tab: "POST",
+      content: (
+        <div>
+          <strong>Intro</strong>
+          <p>Create a short url by given info.</p>
+          <br/>
+          <strong>Params</strong>
+          <ul>
+            <li>url: The url that will be encoded to a short url.</li>
+            <li>short_url: The short url that you want get.</li>
+            <li>digit: The short url's length that you want to get.</li>
+            <li>expired_timestamp: The python format timestamp to specify a expired time.</li>
+            <li>password: The password of the short_url.</li>
+          </ul>
+        </div>
+      )
+    }
+  ]
+};
 
 
 export class ApiDoc extends Component {
+  state = {
+    key: docContent.content[0].key
+  };
+
+  onTabChange = (key, type) => {
+    this.setState({
+      [type]: key
+    })
+  };
+
   render() {
-    return "api doc";
+    return (
+      <InputBox inputBoxIconClassName="apiDocInputBoxIcon">
+        <Card
+          title={docContent.title}
+          tabList={docContent.content}
+          activeTabKey={this.state.key}
+          onTabChange={key => this.onTabChange(key, 'key')}
+          style={{textAlign: "left"}}
+        >
+          {
+            docContent.content.filter(o => o.key === this.state.key)[0].content
+          }
+        </Card>
+      </InputBox>
+    )
   }
 }
